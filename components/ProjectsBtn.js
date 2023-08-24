3f27a6fac3480fe1b693ba3e1edc3a5a3aab91c7
WNagorny <walery.nagorny@gmail.com>
2023-08-24 16:48:38 +0200

    adding animation on large-screen

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -5,7 +5,7 @@ import { HiArrowRight } from 'react-icons/hi2'
 
 const ProjectsBtn = () => {
 	return (
-		<div className='mx-auto'>
+		<div className='mx-auto xl:mx-0'>
 			<Link href={'/work'} className='relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'>
 				<Image
 					className='animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]'
--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,53 @@ import ParticlesContainer from '../components/ParticlesContainer'
 import ProjectsBtn from '../components/ProjectsBtn'
 import Avatar from '../components/Avatar'
 
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
-import {fadeIn} from '../variants'
-
+import { fadeIn } from '../variants'
 
 const Home = () => {
-  return (
+	return (
+		<div className='bg-primary/60 h-full'>
+			<div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30'>
+				<div className='text-center flex flex-col justify-center xl:pt40 xl:text-left h-full container mx-auto'>
+					<motion.h1
+						variants={fadeIn('down', 0.2)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='h1'
+					>
+						Transforming Ideas <br /> {''} Into{' '}
+						<span className='text-accent'>Digital Reality</span>{' '}
+					</motion.h1>
+					<motion.p
+						variants={fadeIn('down', 0.3)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+					>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni id
+						amet perferendis rem inventore delectus!
+					</motion.p>
+					<div className='flex justify-center xl:hidden relative'>
+						<ProjectsBtn />
+					</div>
+					<motion.div
+						variants={fadeIn('down', 0.4)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='hidden xl:flex'
+					>
+						<ProjectsBtn />
+					</motion.div>
+				</div>
+			</div>
 
-    <div className='bg-primary/60 h-full'>
-      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30'>
-        <div className='text-center flex flex-col justify-center xl:pt40 xl:text-left h-full container mx-auto'> 
-          <h1 className='h1'>Transforming Ideas <br /> {''} Into <span className='text-accent'>Digital Reality</span> </h1>
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni id amet perferendis rem inventore delectus!</p>
-          <div className='flex justify-center xl:hidden relative'>
-            <ProjectsBtn/>
-          </div>
-        </div>
-      </div>
+			<div>image</div>
+		</div>
+	)
+}
 
-
-      <div>image</div>
-    </div>
-
-  )
-};
-
-export default Home;
+export default Home
