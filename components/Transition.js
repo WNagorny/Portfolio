aa68858b760936bf2a41c61c1efc28121b0a0b7f
WNagorny <walery.nagorny@gmail.com>
2023-08-24 11:56:39 +0200

    finished trnasition-effect

--- components/Transition.js
+++ components/Transition.js
@@ -18,9 +18,11 @@ const transitionVariants = {
 const Transition = () => {
 	return (
 		<>
-			<motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]' variants={transitionVariants} initial="initial" animate="animate" exit="exit" transition={{delay: 0.1, duration: 0.7, ease: 'easeInOut'}}>1</motion.div>
-			<div>2</div>
-			<div>3</div>
+			<motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]' variants={transitionVariants} initial="initial" animate="animate" exit="exit" transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}>1</motion.div>
+
+			<motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]' variants={transitionVariants} initial="initial" animate="animate" exit="exit" transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}>2</motion.div>
+
+			<motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]' variants={transitionVariants} initial="initial" animate="animate" exit="exit" transition={{delay: 0.6, duration: 0.7, ease: 'easeInOut'}}>3</motion.div>
 		</>
 	)
 }
