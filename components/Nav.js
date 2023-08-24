da23724f32afa2f5fd5653892d9081a86b91faeb
WNagorny <walery.nagorny@gmail.com>
2023-08-24 11:03:27 +0200

    added style to Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,33 +1,53 @@
 // icons
 import {
-  HiHome,
-  HiUser,
-  HiViewColumns,
-  HiRectangleGroup,
-  HiChatBubbleBottomCenterText,
-  HiEnvelope,
-} from 'react-icons/hi2';
+	HiHome,
+	HiUser,
+	HiViewColumns,
+	HiRectangleGroup,
+	HiChatBubbleBottomCenterText,
+	HiEnvelope,
+} from 'react-icons/hi2'
+
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
-  {
-    name: 'testimonials',
-    path: '/testimonials',
-    icon: <HiChatBubbleBottomCenterText />,
-  },
-  {
-    name: 'contact',
-    path: '/contact',
-    icon: <HiEnvelope />,
-  },
-];
+	{ name: 'home', path: '/', icon: <HiHome /> },
+	{ name: 'about', path: '/about', icon: <HiUser /> },
+	{ name: 'services', path: '/services', icon: <HiRectangleGroup /> },
+	{ name: 'work', path: '/work', icon: <HiViewColumns /> },
+	{
+		name: 'testimonials',
+		path: '/testimonials',
+		icon: <HiChatBubbleBottomCenterText />,
+	},
+	{
+		name: 'contact',
+		path: '/contact',
+		icon: <HiEnvelope />,
+	},
+]
 
 const Nav = () => {
-  return <nav>nav</nav>;
-};
+	const router = useRouter()
+	const pathname = router.pathname
 
-export default Nav;
+	return (
+		<nav className='flex flex-col  items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+			{/* inner */}
+
+			<div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+				{navData.map((link, index) => {
+					return (
+						<Link href={link.path} key={index}>
+							{link.icon}
+						</Link>
+					)
+				})}
+			</div>
+		</nav>
+	)
+}
+
+export default Nav
