9cb65770fd279a7231fb1f12a819e91f1f44ad20
WNagorny <walery.nagorny@gmail.com>
2023-08-24 16:30:23 +0200

    styled text and animation to Home page

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,23 @@
-const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
-};
+import Link from 'next/link'
+import Image from 'next/image'
 
-export default ProjectsBtn;
+import { HiArrowRight } from 'react-icons/hi2'
+
+const ProjectsBtn = () => {
+	return (
+		<div className='mx-auto'>
+			<Link href={'/work'} className='relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'>
+				<Image
+					className='animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]'
+					src={'/rounded-text.png'}
+					width={141}
+					height={148}
+					alt=''
+				/>
+				<HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-translate duration-300' />
+			</Link>
+		</div>
+	)
+}
+
+export default ProjectsBtn
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import ParticlesContainer from '../components/ParticlesContainer'
+import ProjectsBtn from '../components/ProjectsBtn'
+import Avatar from '../components/Avatar'
+
+import {motion} from 'framer-motion'
+
+import {fadeIn} from '../variants'
+
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+
+    <div className='bg-primary/60 h-full'>
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30'>
+        <div className='text-center flex flex-col justify-center xl:pt40 xl:text-left h-full container mx-auto'> 
+          <h1 className='h1'>Transforming Ideas <br /> {''} Into <span className='text-accent'>Digital Reality</span> </h1>
+          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni id amet perferendis rem inventore delectus!</p>
+          <div className='flex justify-center xl:hidden relative'>
+            <ProjectsBtn/>
+          </div>
+        </div>
+      </div>
+
+
+      <div>image</div>
+    </div>
+
+  )
 };
 
 export default Home;
