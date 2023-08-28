48a7e201e931c71d0416c734ad35996b8b19eb0e
WNagorny <walery.nagorny@gmail.com>
2023-08-28 15:28:42 +0200

    added data

--- components/Avatar.js
+++ components/Avatar.js
@@ -7,7 +7,7 @@ import Image from "next/image"
 const Avatar = () => {
   return (
   <div classname="hidden xl:flex xl:max-w-none">
-    <Image src={'/avatar.png'} width={737} height={678} alt="" className="w-full h-full"/>
+    <Image src={'/ja.png'} width={737} height={678} alt="" className="w-full h-full"/>
   </div>
   )
 };
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,111 +1,136 @@
-
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 // icons
 import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
-} from "react-icons/fa";
+	FaHtml5,
+	FaCss3,
+	FaJs,
+	FaReact,
+	FaWordpress,
+	FaFigma,
+} from 'react-icons/fa'
 
 import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from "react-icons/si";
-
+	SiNextdotjs,
+	SiFramer,
+	SiAdobexd,
+	SiAdobephotoshop,
+} from 'react-icons/si'
 
 //  data
 const aboutData = [
-  {
-    title: 'skills',
-    info: [
-      {
-        title: 'Web Development',
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
-        ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
-      },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
-      },
-    ],
-  },
-];
+	{
+		title: 'skills',
+		info: [
+			{
+				title: 'Web Development',
+				icons: [
+					<FaHtml5 />,
+					<FaCss3 />,
+					<FaJs />,
+					<FaReact />,
+					<SiNextdotjs />,
+					<SiFramer />,
+					<FaWordpress />,
+				],
+			},
+			{
+				title: 'UI/UX Design',
+				icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+			},
+		],
+	},
+	{
+		title: 'awards',
+		info: [
+			{
+				title: 'Webby Awards - Honoree',
+				stage: '2011 - 2012',
+			},
+			{
+				title: 'Adobe Design Achievement Awards - Finalist',
+				stage: '2009 - 2010',
+			},
+		],
+	},
+	{
+		title: 'experience',
+		info: [
+			{
+				title: 'UX/UI Designer - XYZ Company',
+				stage: '2012 - 2023',
+			},
+			{
+				title: 'Web Developer - ABC Agency',
+				stage: '2010 - 2012',
+			},
+			{
+				title: 'Intern - DEF Corporation',
+				stage: '2008 - 2010',
+			},
+		],
+	},
+	{
+		title: 'credentials',
+		info: [
+			{
+				title: 'Web Development - ABC University, LA, CA',
+				stage: '2011',
+			},
+			{
+				title: 'Computer Science Diploma - AV Technical Institute',
+				stage: '2009',
+			},
+			{
+				title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+				stage: '2006',
+			},
+		],
+	},
+]
 
-// components 
+// components
 
-// import Avatar from '../../components/Avatar'
-import Circles from '../../components/Circles' 
+import Avatar from '../../components/Avatar'
+import Circles from '../../components/Circles'
 
+// framer motion
 
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
 
 const About = () => {
-  const [index, setIndex] = useState(0)
+	const [index, setIndex] = useState(0)
+  console.log(index)
 
-  return(
-    <div>
-      <Circles/>
-    </div>
-  )
-};
+	return (
+		<div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+			<Circles />
+			<motion.div
+				variants={fadeIn('right', 0.2)}
+				initial='hidden'
+				animate='show'
+				exit='hidden'
+				className='hidden xl:flex absolute bottom-0 -left-[370px]'
+			>
+				<Avatar />
+			</motion.div>
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-9'>
+        <div>text</div>
+        <div>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemInidex) => {
+              return (
+                <div key={itemInidex} className={` ${index === itemInidex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-2xl relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                onClick={() => setIndex(itemInidex)}>{item.title}</div>
+              )
+            })}
+          </div>
 
-export default About;
+        </div>
+      </div>
+		</div>
+	)
+}
+
+export default About
