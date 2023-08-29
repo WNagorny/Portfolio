037418813d7ce8e33232e9a9a3232e3fc870c92c
WNagorny <walery.nagorny@gmail.com>
2023-08-29 11:09:51 +0200

    added motion animation

--- pages/about/index.js
+++ pages/about/index.js
@@ -119,53 +119,91 @@ const About = () => {
 				<Avatar />
 			</motion.div>
 			<div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-
-{/* text */}
+				{/* text */}
 
 				<div className='flex-1 flex flex-col justify-center'>
-					<h2 className='h2'>Captivating <span className='text-accent'>stories</span> bith magnificent designs.</h2>
-					<p className='max-w[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2  xl:px-0'>10 years ago, I began freelancing as a developer. Since then, I have done remote work for agencies, consulted fir startups, and collaborated on digital priducts for bussines</p>
+					<motion.h2
+						variants={fadeIn('right', 0.2)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='h2'
+					>
+						Captivating <span className='text-accent'>stories</span> bith
+						magnificent designs.
+					</motion.h2>
+					<motion.p
+						variants={fadeIn('right', 0.4)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2  xl:px-0'
+					>
+						10 years ago, I began freelancing as a developer. Since then, I have
+						done remote work for agencies, consulted fir startups, and
+						collaborated on digital priducts for bussines
+					</motion.p>
 
-{/* counters */}
+					{/* counters */}
 
-					<div className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'>
+					<motion.div
+						variants={fadeIn('right', 0.6)}
+						initial='hidden'
+						animate='show'
+						exit='hidden'
+						className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'
+					>
 						<div className='flex flex-1 xl:gap-x-6'>
-				{/* experience */}
+							{/* experience */}
 							<div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
 								<div className='text-2xl xl:text-4xl font-extrabold text-accent  mb-2'>
-									<CountUp start={0} end={10} duration={7}/> +
+									<CountUp start={0} end={10} duration={7} /> +
 								</div>
-								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>Years of experince</div>
+								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>
+									Years of experince
+								</div>
 							</div>
-				{/* cleints */}
+							{/* cleints */}
 							<div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
 								<div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
-									<CountUp start={0} end={250} duration={7}/> +
+									<CountUp start={0} end={250} duration={7} /> +
 								</div>
-								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>Satisfied clients</div>
+								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>
+									Satisfied clients
+								</div>
 							</div>
-				{/* projects */}
+							{/* projects */}
 							<div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
 								<div className='text-2xl xl:text-4xl font-extrabold text-accent  mb-2'>
-									<CountUp start={0} end={650} duration={7}/> +
+									<CountUp start={0} end={650} duration={7} /> +
 								</div>
-								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>Finished projects</div>
+								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>
+									Finished projects
+								</div>
 							</div>
-				{/* awards */}
+							{/* awards */}
 							<div className='relative flex-1'>
 								<div className='text-2xl xl:text-4xl font-extrabold text-accent  mb-2'>
-									<CountUp start={0} end={8} duration={7}/> +
+									<CountUp start={0} end={8} duration={7} /> +
 								</div>
-								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>Winnig awards</div>
+								<div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] my-2'>
+									Winnig awards
+								</div>
 							</div>
 						</div>
-					</div>
+					</motion.div>
 				</div>
 
-{/* info */}	
+				{/* info */}
 
-				<div className='flex flex-col w-full xl:max-w-[45%] h-[580px]'>
-					<div className='flex gap-x-4 md:gap-x-9 text-lg md:text-2xl xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+				<motion.div
+					variants={fadeIn('left', 0.4)}
+					initial='hidden'
+					animate='show'
+					exit='hidden'
+					className='flex flex-col w-full xl:max-w-[45%] h-[580px]'
+				>
+					<div className='flex gap-x-3 md:gap-x-8 text-lg md:text-2xl xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
 						{aboutData.map((item, itemIndex) => {
 							return (
 								<div
@@ -185,20 +223,27 @@ const About = () => {
 					<div className='py-4 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4  items-center xl:items-start'>
 						{aboutData[index].info.map((item, itemIndex) => {
 							return (
-								<div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-2-max gap-x-2 items-center  text-lg xl:text-xl xxl:text-2xl text-white/60'>
+								<div
+									key={itemIndex}
+									className='flex-1 flex flex-col md:flex-row max-2-max gap-x-2 items-center  text-lg xl:text-xl xxl:text-2xl text-white/60'
+								>
 									<div className='font-light mb-2 md:mb:0 '>{item.title}</div>
 									<div className='hidden xl:flex'>-</div>
 									<div>{item.stage}</div>
 									<div className='flex gap-x-4'>
 										{item.icons?.map((icon, itemIndex) => {
-											return <div key={itemIndex} className='text-3xl text-white'>{icon}</div>
+											return (
+												<div key={itemIndex} className='text-3xl text-white'>
+													{icon}
+												</div>
+											)
 										})}
 									</div>
 								</div>
 							)
 						})}
 					</div>
-				</div>
+				</motion.div>
 			</div>
 		</div>
 	)
