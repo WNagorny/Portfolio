463351bf46531dc24fee005be6dc0a7d1abda1b3
WNagorny <walery.nagorny@gmail.com>
2023-08-29 16:47:47 +0200

    configuration Swiper part 1

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,44 +1,74 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
 // icons
 import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
+	RxCrop,
+	RxPencil2,
+	RxDesktop,
+	RxReader,
+	RxRocket,
+	RxArrowTopRight,
+} from 'react-icons/rx'
 
+// import required modules
+import { FreeMode, Pagination } from 'swiper'
 
 // data
 const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-];
+	{
+		icon: <RxCrop />,
+		title: 'Branding',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+	},
+	{
+		icon: <RxPencil2 />,
+		title: 'Design',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+	},
+	{
+		icon: <RxDesktop />,
+		title: 'Development',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+	},
+	{
+		icon: <RxReader />,
+		title: 'Copywriting',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+	},
+	{
+		icon: <RxRocket />,
+		title: 'SEO',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+	},
+]
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
-};
+	return (
+		<Swiper
+			breakpoints={{
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 15,
+				},
+				640: {
+					slidesPerView: 3,
+					spaceBetween: 15,
+				},
+			}}
+      freeMode ={true}
+      pagination = {{
+        clickable: true
+      }}
+      modules={[FreeMode,Pagination]}
+      className='h-[240px] sm:h-[340px]'
+		>
+			Service Slider
+		</Swiper>
+	)
+}
 
-export default ServiceSlider;
+export default ServiceSlider
