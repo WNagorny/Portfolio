2f7c037cefae901f1807cebc52e3889897de6ae5
WNagorny <walery.nagorny@gmail.com>
2023-08-30 17:12:27 +0200

    adding workslider compoennt

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,5 +1,24 @@
+// import required modules
+import { Pagination } from 'swiper'
+
+import Image from 'next/image'
+
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import Swiper styles
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
+// icons
+import {
+  BsArrowRight,
+
+} from 'react-icons/bs'
+
 // data
-const workSlides = {
+const workSlider = {
   slides: [
     {
       images: [
@@ -45,7 +64,35 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
-};
+	return (
+		<Swiper
+			spaceBetween={10}
+			pagination={{
+				clickable: true,
+			}}
+			modules={[Pagination]}
+			className='h-[280px] sm:h-[480px]'
+		>
+			{workSlider.slides.map((slide, index) => {
+				return (
+					<SwiperSlide key={index}>
+            <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+              {slide.images.map((image, index) => {
+                return (
+                  <div key={index} className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+                    <div>
+     {/* image */}
+                    <Image src={image.path} width={500} height={300} alt='' />
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+					</SwiperSlide>
+				)
+			})}
+		</Swiper>
+	)
+}
 
-export default WorkSlider;
+export default WorkSlider
