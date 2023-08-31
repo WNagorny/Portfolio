04943ea7e90fd26660aab4ea7e963d475728121b
WNagorny <walery.nagorny@gmail.com>
2023-08-31 20:33:20 +0200

    remove capitalize in form

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -22,26 +22,41 @@ const Contact = () => {
 						Let's <span className='text-accent'>connect.</span>
 					</motion.h2>
 
-					{/* form */} 
+					{/* form */}
 
-
-					<motion.form className='flex-1 flex flex-col gap-6 w-full mx-auto'>
-
+					<motion.form
+						className='flex-1 flex flex-col gap-6 w-full mx-auto'
+						action='https://formsubmit.co/walery.nagorny@gmail.com'
+						method='POST'
+					>
 						{/* input group */}
 
 						<div className='flex gap-x-6 w-full'>
-							<input type='text' placeholder='name' className='input' />
-							<input type='text' placeholder='email' className='input' />
+							<input type='text' placeholder='name' className='input'  required />
+							<input
+								type='text'
+								placeholder='email'
+								className='input'
+
+
+                required
+							/>
+            {/* <input type="text" /> */}
 						</div>
 						<input type='text' placeholder='subject' className='input' />
-						<textarea placeholder='message' className='textarea'></textarea>
-						<button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
+						<textarea placeholder='message' className='textarea' required></textarea>
+						<button
+							type='submit'
+							className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'
+						>
 							<span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
 								Let's talk
 							</span>
 							<BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
 						</button>
 					</motion.form>
+
+          
 				</div>
 			</div>
 		</div>
