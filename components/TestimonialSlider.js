09c9b00f28d7d35399eb666d1241a04e957b060b
WNagorny <walery.nagorny@gmail.com>
2023-08-31 17:17:21 +0200

    finished Testimonials component

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,29 @@
 // testimonial data
 const testimonialData = [
-  {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-  {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-  {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-];
+	{
+		image: '/t-avt-1.png',
+		name: 'Anne Smith',
+		position: 'Customer',
+		message:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+	},
+	{
+		image: '/t-avt-2.png',
+		name: 'Jane Doe',
+		position: 'Customer',
+		message:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+	},
+	{
+		image: '/t-avt-3.png',
+		name: 'Jhon Doe',
+		position: 'Customer',
+		message:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+	},
+]
 
 // import required modules
-import { Pagination } from 'swiper'
 
 import Image from 'next/image'
 
@@ -32,50 +31,59 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 
 // import Swiper styles
 import 'swiper/css'
-import 'swiper/css/free-mode'
 import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import { Navigation, Pagination } from 'swiper'
 
 // icons
 import { FaQuoteLeft } from 'react-icons/fa'
 
 // data
 
-
 const TestimonialSlider = () => {
 	return (
 		<Swiper
-			spaceBetween={10}
+			navigation={true}
 			pagination={{
 				clickable: true,
 			}}
-			modules={[Pagination]}
-			className='h-[340px] sm:h-[400px] md:h-[480px] lg:h-[600px] xl:h-[510px] xxl:h-[630px]'
+			modules={[Navigation, Pagination]}
+			// className='h-[340px] sm:h-[400px] md:h-[480px] lg:h-[600px] xl:h-[510px] xxl:h-[630px]'
+      className='h-[400px]'
 		>
-			{ testimonialData.map((person, index) => {
+			{testimonialData.map((person, index) => {
 				return (
 					<SwiperSlide key={index}>
-            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
-{/* avatar, name, position */}
-              <div className='w-full max-w-[300px] flex-flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
-                <div className='flex flex-col jc text-center'>
-            {/* avatar */}
-                  <div>avatar image</div>
-            {/* name */}
-                  <div>name</div>
-            {/* position */}
-                  <div>position</div>
-                </div>
-              </div>
-{/* quote & message */}
-              <div className=' flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
-            {/* quote icon */}
-            <div className='mb-4'>
-              <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0'/>
-            </div>
-            {/* quote message */}
-                <div className ="xl:text-lg text-center md:text-left">{person.message}</div>
-              </div>
-            </div>
+						<div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+							{/* avatar, name, position */}
+							<div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+								<div className='flex flex-col justify-center text-center'>
+									{/* avatar */}
+									<div className='mb-2 mx-auto'>
+										<Image src={person.image} width={100} height={100} alt='' />
+									</div>
+
+									{/* name */}
+									<div className='text-lg'>{person.name}</div>
+									{/* position */}
+									<div className='text-[12px] uppercase font-extralight tracking-widest'>
+										{person.position}
+									</div>
+								</div>
+							</div>
+							{/* quote & message */}
+							<div className=' flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+								{/* quote icon */}
+								<div className='mb-4'>
+									<FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
+								</div>
+								{/* quote message */}
+								<div className='xl:text-lg text-center md:text-left'>
+									{person.message}
+								</div>
+							</div>
+						</div>
 					</SwiperSlide>
 				)
 			})}
@@ -84,4 +92,3 @@ const TestimonialSlider = () => {
 }
 
 export default TestimonialSlider
-
